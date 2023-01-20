ae42de56fa7e7d962da80d934d090dc03faa0ca6
derFreemaker <89133130+derFreemaker@users.noreply.github.com>
2023-01-20 01:41:08 +0100

    Added test programs

---
+++ TestRecieveServer/Main.lua
@@ -0,0 +1,44 @@
+local Main = {}
+Main.__index = Main
+
+Main.Logger = {}
+
+Main.SetupFilesTree = {
+    "",
+    IsFolder = true,
+    {
+        "shared",
+        IsFolder = true,
+        IgnoreDownload = true,
+        {"Logger.lua"}
+    },
+    {
+        "libs",
+        IsFolder = true,
+        {"Event.lua"},
+        {"Serializer.lua"},
+        {"EventPullAdapter.lua"},
+        {"NetworkCard.lua"}
+    }
+}
+
+function Main:Configure()
+    self.Logger:LogInfo("called configure function")
+end
+
+function Main:Test(signalName, signalSender, data)
+    print("Got Message to: "..tostring(signalName))
+end
+
+function Main:Run()
+    ModuleLoader.GetModule("EventPullAdapter"):Initialize(true)
+    local networkCard = computer.getPCIDevices(findClass("NetworkCard"))[1]
+    local netClient = ModuleLoader.GetModule("NetworkCard").new(true, networkCard)
+    netClient:OpenPort(42)
+
+    netClient:AddListener("Test", self.Test, true)
+    local eventPullAdapter = ModuleLoader.GetModule("EventPullAdapter")
+    eventPullAdapter:Run()
+end
+
+return Main
---
+++ TestSendServer/Main.lua
@@ -0,0 +1,36 @@
+local Main = {}
+Main.__index = Main
+
+Main.Logger = {}
+
+Main.SetupFilesTree = {
+    "",
+    IsFolder = true,
+    {
+        "shared",
+        IsFolder = true,
+        IgnoreDownload = true,
+        {"Logger.lua"}
+    },
+    {
+        "libs",
+        IsFolder = true,
+        {"Event.lua"},
+        {"Serializer.lua"},
+        {"EventPullAdapter.lua"},
+        {"NetworkCard.lua"}
+    }
+}
+
+function Main:Configure()
+    self.Logger:LogInfo("called configure function")
+end
+
+function Main:Run()
+    ModuleLoader.GetModule("EventPullAdapter"):Initialize(true)
+    local networkCard = computer.getPCIDevices(findClass("NetworkCard"))[1]
+    local netClient = ModuleLoader.GetModule("NetworkCard").new(true, networkCard)
+    netClient:SendBroadCast(42, "Test", nil)
+end
+
+return Main
