fad83b43a5c277b612422efeb7b4fa79a5d6771e
derFreemaker <89133130+derFreemaker@users.noreply.github.com>
2023-03-17 16:16:27 +0100

    forget it

--- FactoryControl/Controller/Main.lua
+++ FactoryControl/Controller/Main.lua
@@ -7,33 +7,33 @@ Main.SetupFilesTree = {
     "",
     {
         "shared",
-        {"Logger.lua"}
+        { "Logger.lua" }
     },
     {
         "libs",
         {
             "NetworkClient",
-            {"NetworkClient.lua"},
-            {"NetworkPort.lua"}
+            { "NetworkClient.lua" },
+            { "NetworkPort.lua" }
         },
         {
             "Api",
-            {"ApiController.lua"},
-            {"ApiClient.lua"}
+            { "ApiController.lua" },
+            { "ApiClient.lua" }
         },
-        {"Event.lua"},
-        {"EventPullAdapter"},
-        {"Serializer.lua"},
+        { "Event.lua" },
+        { "EventPullAdapter" },
+        { "Serializer.lua" },
     },
     {
         "FactoryControl",
         {
             "Entities",
-            {"Controller.lua"}
+            { "Controller.lua" }
         },
         {
             "FactoryControlApiClient",
-            {"FactoryControlApiClient.lua"}
+            { "FactoryControlApiClient.lua" }
         }
     }
 }
@@ -43,13 +43,21 @@ Main.FactoryControlApiClient = {}
 function Main:Configure()
     require("EventPullAdapter"):Initialize(self._logger)
     local netClient = require("NetworkClient").new(self._logger)
-    local apiClient = require("ApiClient").new(netClient, Config.ServerIPAddress, Config.ServerPort, Config.ReturnPort)
+    local apiClient = require("ApiClient").new(
+        netClient,
+        Config.ServerIPAddress,
+        Config.ServerPort,
+        Config.ReturnPort)
     self.FactoryControlApiClient = require("FactoryControlApiClient").new(apiClient)
 end
 
 function Main:Run()
     self._logger:LogInfo("adding controller...")
-    local result = self.FactoryControlApiClient:AddController({IPAddress="TestIPAddress", Name="Test", Category="Test"})
+    local result = self.FactoryControlApiClient:AddController({
+        IPAddress = "TestIPAddress",
+        Name = "Test",
+        Category = "Test"
+    })
     self._logger:LogInfo("added controllers")
     self._logger:LogInfo(result.Body.Success)
     self._logger:LogInfo(#result.Body.Result)
