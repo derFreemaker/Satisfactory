50b9ebd5de36954e3f372f19c5fb256f6c1cea38
derFreemaker <89133130+derFreemaker@users.noreply.github.com>
2023-04-28 20:22:40 +0200

    changing bundler from lua using to c#

---
+++ Lua-Bundler/BundlePart.cs
@@ -0,0 +1,44 @@
+﻿namespace Lua_Bundler
+{
+    internal class BundlePart
+    {
+        public Guid Id { get; }
+        public string Location { get; }
+        public bool IsDirectory { get; }
+        public string FullName { get; }
+        public string Name { get; }
+        public BundlePart[] Childs { get; private set; }
+        public BundlePart? Parent { get; }
+
+        public BundlePart(Guid id, string location, bool isDirectory, BundlePart? parent = null)
+        {
+            Id = id;
+            Location = location;
+            IsDirectory = isDirectory;
+
+            FullName = IsDirectory ? Path.GetDirectoryName(location) ?? throw new DirectoryNotFoundException($"Could not find '{location}'") : Path.GetFileName(location);
+            Name = IsDirectory ? FullName : Path.GetFileNameWithoutExtension(location);
+
+            Childs = Array.Empty<BundlePart>();
+            Parent = parent;
+        }
+
+        private void BuildChilds(bool buildAllChilds = false)
+        {
+            if (!IsDirectory) return;
+
+            var childs = new List<BundlePart>();
+            foreach (var child in Directory.GetDirectories(Location, "*", SearchOption.TopDirectoryOnly))
+            {
+                var bundlePart = new BundlePart(Guid.NewGuid(), child, true, this);
+                childs.Add(bundlePart);
+                if (buildAllChilds) bundlePart.BuildChilds(buildAllChilds);
+            }
+
+            foreach (var child in Directory.GetFiles(Location, "*", SearchOption.TopDirectoryOnly))
+                childs.Add(new BundlePart(Guid.NewGuid(), child, false, this));
+
+            Childs = childs.ToArray();
+        }
+    }
+}
---
+++ Lua-Bundler/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lua_Bundler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
