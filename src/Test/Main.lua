5f5f6cac52da42ccb67e70514dc68fe5b9c7bb01
derFreemaker <ralf.rose.oskar@gmail.com>
2023-08-06 20:38:45 +0200

    small change

--- src/Test/Main.lua
+++ src/Test/Main.lua
@@ -1,15 +1,13 @@
 ---@class TestMain : Main
----@field private logger Github_Loading.shared.Logger
 local Test = {}
 
----@param logger Github_Loading.shared.Logger
-function Test:Configure(logger)
-    self.logger = logger
+function Test:Configure()
+    self.Logger:LogDebug("called configure function")
 end
 
 ---@return integer
 function Test:Run()
-    self.logger:LogInfo("Running Main Module")
+    self.Logger:LogInfo("Running Main Module")
     local file1 = require("Test.File1")
     print(file1:Test())
     return 0
