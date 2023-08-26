69aaf3e97d7c1023c55c2dae284b445266271528
Freemaker <89133130+derFreemaker@users.noreply.github.com>
2023-08-26 04:18:22 +0200

    clearing log file again

--- Github-Loading/Loader.lua
+++ Github-Loading/Loader.lua
@@ -247,7 +247,7 @@ function Loader:setupLogger(logLevel)
     self.Logger.OnLog:AddListener(Listener.new(logFile))
     self.Logger.OnClear:AddListener(Listener.new(clear))
     self.Logger:setErrorLogger()
-    self.Logger:FreeLine(2)
+    self.Logger:Clear()
     self.Logger:Log("###### LOG START: ".. tostring(table.pack(computer.magicTime())[2]) .." ######", 10)
     self.Logger.OnLog:AddListener(Listener.new(logConsole))
 end
