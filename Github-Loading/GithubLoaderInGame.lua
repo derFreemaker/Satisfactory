ed0eb8a4faf9c1472af62c52b9215dcc761ee976
Freemaker <89133130+derFreemaker@users.noreply.github.com>
2023-09-10 00:38:13 +0200

    Update GithubLoaderInGame.lua

--- Github-Loading/GithubLoaderInGame.lua
+++ Github-Loading/GithubLoaderInGame.lua
@@ -15,7 +15,7 @@ local programForceDownload = false
 -- to define any config variables
 Config = {}
 
-local BaseUrl = "https://raw.githubusercontent.com/derFreemaker/Satisfactory/main"
+local BaseUrl = "https://raw.githubusercontent.com/derFreemaker/Satisfactory/staging"
 
 local showDriveUUID = false
 
