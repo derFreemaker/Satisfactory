6d3b81ca40fb7fd9d0fdbe9bcf921513814e47d8
Freemaker <89133130+derFreemaker@users.noreply.github.com>
2023-09-17 20:19:38 +0200

    dev

--- Github-Loading/GithubLoaderInGame.lua
+++ Github-Loading/GithubLoaderInGame.lua
@@ -15,91 +15,98 @@ local programForceDownload = false
 -- to define any config variables
 Config = {}
 
-local BaseUrl = "https://raw.githubusercontent.com/derFreemaker/Satisfactory/Lua-Bundler"
+local BaseUrl = 'https://raw.githubusercontent.com/derFreemaker/Satisfactory/dev'
 
 local showDriveUUID = false
 
 -- ########## Don't touch that ########## --
-local LoaderFilesUrl = BaseUrl .. "/Github-Loading"
-local LoaderUrl = LoaderFilesUrl .. "/Loader.lua"
-local LoaderFilesPath = "Loader"
-local LoaderPath = LoaderFilesPath .. "/Loader.lua"
+local LoaderFilesUrl = BaseUrl .. '/Github-Loading'
+local LoaderUrl = LoaderFilesUrl .. '/Loader.lua'
+local LoaderFilesPath = 'Loader'
+local LoaderPath = LoaderFilesPath .. '/Loader.lua'
 
 ---@type FicsIt_Networks.Components.FINComputerMod.InternetCard_C
-local internetCard = computer.getPCIDevices(findClass("FINInternetCard"))[1]
+local internetCard = computer.getPCIDevices(findClass('FINInternetCard'))[1]
 if not internetCard then
-    computer.beep(0.2)
-    error("No internet-card found!")
-    return
+	computer.beep(0.2)
+	error('No internet-card found!')
+	return
 end
-filesystem.initFileSystem("/dev")
-local drive = ""
-for _, child in pairs(filesystem.childs("/dev")) do
-    if not (child == "serial") then
-        drive = child
-        break
-    end
+filesystem.initFileSystem('/dev')
+local drive = ''
+for _, child in pairs(filesystem.childs('/dev')) do
+	if not (child == 'serial') then
+		drive = child
+		break
+	end
 end
 if drive:len() < 1 then
-    computer.beep(0.2)
-    error("Unable to find filesystem to load on! Insert a drive or floppy.")
-    return
+	computer.beep(0.2)
+	error('Unable to find filesystem to load on! Insert a drive or floppy.')
+	return
 end
-filesystem.mount("/dev/" .. drive, "/")
+filesystem.mount('/dev/' .. drive, '/')
 
 if showDriveUUID then
-    print("[Computer] DEBUG mounted filesystem on drive: " .. drive)
+	print('[Computer] DEBUG mounted filesystem on drive: ' .. drive)
 end
 
 ---@return boolean restart
 local function Run()
-    if not filesystem.exists(LoaderFilesPath) then
-        filesystem.createDir(LoaderFilesPath)
-    end
-    if not filesystem.exists(LoaderPath) or loaderForceDownload then
-        print("[Computer] INFO downloading Github Loader...")
-        local req = internetCard:request(LoaderUrl, "GET", "")
-        repeat until req:canGet()
-        local _, libdata = req:get()
-        ---@cast libdata string
-        local file = filesystem.open(LoaderPath, "w")
-        assert(file, "Unable to open file: '" .. LoaderPath .. "'")
-        file:write(libdata)
-        file:close()
-        print("[Computer] INFO downloaded Github Loader")
-    end
+	if not filesystem.exists(LoaderFilesPath) then
+		filesystem.createDir(LoaderFilesPath)
+	end
+	if not filesystem.exists(LoaderPath) or loaderForceDownload then
+		print('[Computer] INFO downloading Github Loader...')
+		local req = internetCard:request(LoaderUrl, 'GET', '')
+		repeat
+		until req:canGet()
+		local _,
+			libdata = req:get()
+		---@cast libdata string
+		local file = filesystem.open(LoaderPath, 'w')
+		assert(file, "Unable to open file: '" .. LoaderPath .. "'")
+		file:write(libdata)
+		file:close()
+		print('[Computer] INFO downloaded Github Loader')
+	end
 
-    -- ######## load Loader Files and initialize ######## --
-    ---@type Github_Loading.Loader
-    local Loader = filesystem.doFile(LoaderPath)
-    assert(Loader, "Unable to load Github Loader")
+	-- ######## load Loader Files and initialize ######## --
+	---@type Github_Loading.Loader
+	local Loader = filesystem.doFile(LoaderPath)
+	assert(Loader, 'Unable to load Github Loader')
 
-    Loader = Loader.new(BaseUrl, LoaderFilesPath, loaderForceDownload, internetCard)
-    Loader:Load(loaderLogLevel)
-    local diffrentVersionFound = Loader:CheckVersion()
-    if diffrentVersionFound then
-        loaderForceDownload = true
-        return true
-    end
+	Loader = Loader.new(BaseUrl, LoaderFilesPath, loaderForceDownload, internetCard)
+	Loader:Load(loaderLogLevel)
+	local diffrentVersionFound = Loader:CheckVersion()
+	if diffrentVersionFound then
+		loaderForceDownload = true
+		return true
+	end
 
-    -- ######## load option ######## --
-    local chosenOption = Loader:LoadOption(option, showExtendOptionDetails)
-    local program, package = Loader:LoadProgram(chosenOption, BaseUrl, programForceDownload)
+	-- ######## load option ######## --
+	local chosenOption = Loader:LoadOption(option, showExtendOptionDetails)
+	local program,
+		package = Loader:LoadProgram(chosenOption, BaseUrl, programForceDownload)
 
-    -- ######## start Program ######## --
-    Loader:Configure(program, package, programLogLevel)
-    Loader:Run(program)
+	-- ######## start Program ######## --
+	Loader:Configure(program, package, programLogLevel)
+	Loader:Run(program)
 
-    return false
+	return false
 end
 
 repeat
-    local thread = coroutine.create(function()
-        coroutine.yield(Run())
-    end)
-    local success, result = coroutine.resume(thread)
-    if not success then
-        print(debug.traceback(thread, result))
-    end
-    coroutine.close(thread)
-until not result or type(result) ~= "boolean"
+	local thread =
+		coroutine.create(
+		function()
+			coroutine.yield(Run())
+		end
+	)
+	local success,
+		result = coroutine.resume(thread)
+	if not success then
+		print(debug.traceback(thread, result))
+	end
+	coroutine.close(thread)
+until not result or type(result) ~= 'boolean'
