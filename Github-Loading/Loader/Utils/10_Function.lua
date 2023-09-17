2331442d6f2f59477535ce3639c2aec60fe0b8bc
Freemaker <89133130+derFreemaker@users.noreply.github.com>
2023-09-17 20:04:09 +0200

    changed some stuff

--- Github-Loading/Loader/Utils/10_Function.lua
+++ Github-Loading/Loader/Utils/10_Function.lua
@@ -1,37 +1,24 @@
 ---@class Utils.Function
 local Function = {}
 
----@param func function
----@param parent any
----@param args any[]
----@return any[] returns
-function Function.DynamicInvoke(func, parent, args)
-    local results
-    if parent ~= nil then
-        results = {func(parent, table.unpack(args))}
-    else
-        results = {func(table.unpack(args))}
-    end
-    return results
-end
-
 ---@param success boolean
 ---@param ... any
 ---@return boolean success, table data
 local function extractSuccess(success, ...)
-    return success, { ... }
+	return success, {...}
 end
 ---@param func function
 ---@param ... any
 ---@return thread thread, boolean success, any[] returns
 function Function.InvokeProtected(func, ...)
-    local function invokeFunc(...)
-        coroutine.yield(func(...))
-    end
-    local thread = coroutine.create(invokeFunc)
-    local results = { coroutine.resume(thread, ...) }
-    local success, filteredResults = extractSuccess(table.unpack(results))
-    return thread, success, filteredResults
+	local function invokeFunc(...)
+		coroutine.yield(func(...))
+	end
+	local thread = coroutine.create(invokeFunc)
+	local results = {coroutine.resume(thread, ...)}
+	local success,
+		filteredResults = extractSuccess(table.unpack(results))
+	return thread, success, filteredResults
 end
 
 return Function
