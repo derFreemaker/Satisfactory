30a44857325f9deb58950801c24fb52b7ac25018
Freemaker <89133130+derFreemaker@users.noreply.github.com>
2023-10-11 21:18:41 +0200

    added Utils.File back for Loader

--- Github-Loading/Loader/Utils/10_File.lua
+++ Github-Loading/Loader/Utils/10_File.lua
@@ -34,4 +34,61 @@ function filesystem.open(path, mode)
     return file
 end
 
+---@alias Utils.File.writeModes
+---|"w" write -> file stream can read and write creates the file if it doesn’t exist
+---|"a" end of file -> file stream can read and write cursor is set to the end of file
+---|"+r" truncate -> file stream can read and write all previous data in file gets dropped
+---|"+a" append -> file stream can read the full file but can only write to the end of the existing file
+
+---@param path string
+---@param mode Utils.File.writeModes
+---@param data string?
+---@param createPath boolean?
+function File.Write(path, mode, data, createPath)
+    data = data or ""
+    createPath = createPath or false
+
+    local fileName = filesystem.path(3, path)
+    local folderPath = path:gsub(fileName, "")
+    if not filesystem.exists(folderPath) then
+        if not createPath then
+            error("folder does not exists: '" .. folderPath .. "'", 2)
+        end
+        filesystem.createDir(folderPath)
+    end
+
+    local file = filesystem.open(path, mode)
+    file:write(data)
+    file:close()
+end
+
+---@param path string
+---@return string?
+function File.ReadAll(path)
+    if not filesystem.exists(path) then
+        return nil
+    end
+    local file = filesystem.open(path, "r")
+    local str = ""
+    while true do
+        local buf = file:read(8192)
+        if not buf then
+            break
+        end
+        str = str .. buf
+    end
+    file:close()
+    return str
+end
+
+---@param path string
+function File.Clear(path)
+    if not filesystem.exists(path) then
+        return
+    end
+    local file = filesystem.open(path, "w")
+    file:write("")
+    file:close()
+end
+
 return File, openFiles
