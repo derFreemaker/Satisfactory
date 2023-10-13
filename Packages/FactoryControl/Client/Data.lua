5baa362a156b77ecd38656bc399bd7f3ac1dda53
Freemaker <89133130+derFreemaker@users.noreply.github.com>
2023-10-13 18:38:50 +0200

    changed Factory Controller to Client

--- Packages/FactoryControl/Client/Data.lua
+++ Packages/FactoryControl/Client/Data.lua
@@ -1,39 +1,9 @@
 ---@meta
 local PackageData = {}
 
-PackageData["FactoryControlController__main"] = {
-    Location = "FactoryControl.Controller.__main",
-    Namespace = "FactoryControl.Controller.__main",
-    IsRunnable = true,
-    Data = [[
-local NetworkClient = require('Net.Core.NetworkClient')
-local FactoryControlRestApiClient = require('FactoryControl.Controller.FactoryControlApiClient')
-local EventPullAdapter = require('Core.Event.EventPullAdapter')
-
----@class FactoryControl.Controller.Main : Github_Loading.Entities.Main
----@field private apiClient FactoryControl.Controller.Client
-local Main = {}
-
-function Main:Configure()
-	EventPullAdapter:Initialize(self.Logger:subLogger('EventPullAdapter'))
-
-	local netClient = NetworkClient(self.Logger:subLogger('NetworkClient'))
-	self.apiClient = FactoryControlRestApiClient(netClient, self.Logger:subLogger('ApiClient'))
-	self.Logger:LogDebug('setup apiClient')
-end
-
-function Main:Run()
-	local result = self.apiClient:CreateController()
-	self.Logger:LogInfo('result: ' .. tostring(result))
-end
-
-return Main
-]]
-}
-
-PackageData["FactoryControlControllerFactoryControlApiClient"] = {
-    Location = "FactoryControl.Controller.FactoryControlApiClient",
-    Namespace = "FactoryControl.Controller.FactoryControlApiClient",
+PackageData["FactoryControlClientClient"] = {
+    Location = "FactoryControl.Client.Client",
+    Namespace = "FactoryControl.Client.Client",
     IsRunnable = true,
     Data = [[
 local ApiClient = require('Net.Rest.Api.Client.Client')
