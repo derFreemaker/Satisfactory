491f1559e3a7119f5bc5d2231e42f040c8aba246
Freemaker <89133130+derFreemaker@users.noreply.github.com>
2023-11-10 23:32:22 +0100

    small changes

--- Github-Loading/Loader.lua
+++ Github-Loading/Loader.lua
@@ -238,9 +238,6 @@ local Loader = {}
 ---@param internetCard FIN.Components.FINComputerMod.InternetCard_C
 ---@return Github_Loading.Loader
 function Loader.new(loaderBaseUrl, loaderBasePath, forceDownload, internetCard)
-	-- //TODO: having fun with computer.promote() may cause problems
-	computer.promote()
-
 	return setmetatable(
 		{
 			m_loaderBaseUrl = loaderBaseUrl,
